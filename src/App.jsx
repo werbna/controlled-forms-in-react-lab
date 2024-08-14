9a7070c0f6fcd3d0a81ad0a2beedb2a3f90135d6
Andrew <andrewchau1993@gmail.com>
2024-08-14 12:21:29 -0700

    finished Bookshelf Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-// src/App.jsx
+
+import { useState } from 'react';
+import './App.css';
+import Bookshelf from './components/BookShelf/Bookshelf';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [books, setBooks] = useState([])
+  const [newBook, setNewBook] = useState({
+    title: '',
+    author: '',
+    genre:'',
+    year:'',
+  })
+
+  const handleInputChange = (event) => {
+    setFormData({})
+  }
+
+  const handleSubmit = (event) => {
+
+  }
+  return (
+    <>
+      <h1>My BookShelf</h1>
+      <Bookshelf />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/BookShelf/Bookshelf.jsx
@@ -0,0 +1,14 @@
+const Bookshelf = () => {
+  return (
+    <>
+      <div className="bookshelfDiv">
+        <div className="formDiv">
+          <h3>Add a Book</h3>
+          {/* Form will go here */}
+        </div>
+        <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+      </div>
+    </>
+  );
+};
+export default Bookshelf;
