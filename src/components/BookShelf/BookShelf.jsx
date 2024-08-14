1fa194b211cccec127b5a04e3f064f172b2aabc8
Andrew <andrewchau1993@gmail.com>
2024-08-14 13:06:44 -0700

    realized i took out the div className and fixed it

--- src/components/BookShelf/BookShelf.jsx
+++ src/components/BookShelf/BookShelf.jsx
@@ -1,47 +1,53 @@
 const BookShelf = ({ books, newBook, handleInputChange, handleSubmit }) => {
+
+  
   return (
-    <div>
-      <h2>Add a New Book</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          value={newBook.title}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="author"
-          placeholder="Author"
-          value={newBook.author}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="genre"
-          placeholder="Genre"
-          value={newBook.genre}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="year"
-          placeholder="Year"
-          value={newBook.year}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Add Book</button>
-      </form>
-      <h2>Your Books</h2>
-      <ul>
-        {books.map((book, index) => (
-          <li key={index}>
-            {book.title} by {book.author} - {book.genre}, {book.year}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <div className="bookshelfDiv">
+  <div className="formDiv">
+    <h3>Add a Book</h3>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        placeholder="Title"
+        value={newBook.title}
+        onChange={handleInputChange}
+      />
+      <input
+        type="text"
+        name="author"
+        placeholder="Author"
+        value={newBook.author}
+        onChange={handleInputChange}
+      />
+      <input
+        type="text"
+        name="genre"
+        placeholder="Genre"
+        value={newBook.genre}
+        onChange={handleInputChange}
+      />
+      <input
+        type="text"
+        name="year"
+        placeholder="Year"
+        value={newBook.year}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Add Book</button>
+    </form>
+  </div>
+  <div className="bookCardsDiv">
+    {books.map((book, index) => (
+      <div key={index} className="bookCard">
+        <h4>{book.title}</h4>
+        <p>{book.author}</p>
+        <p>{book.genre}</p>
+        <p>{book.year}</p>
+      </div>
+    ))}
+  </div>
+</div>
   );
 };
 export default BookShelf;
